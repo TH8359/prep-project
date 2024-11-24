f53cb8f788ade7e8ea53914e0919d71436b4c6a2
Tyler Hogue <hogue73@embarqmail.com>
2024-11-24 16:29:22 -0500

    change and center text

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "My Top 5 Favorite Colors";
 document.querySelector("body").appendChild(h2);
